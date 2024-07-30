6014d36d902c71dc803e767bd8caef3f0929824a
thomkainos <thomas.phan@kainos.com>
2024-07-30 16:08:59 -0400

    feat(database): added scripts for roel and user table

---
+++ src/main/resources/db/migration/V3__create_sys_role_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `system_role` (
+    sys_role_id TINYINT PRIMARY KEY AUTO_INCREMENT,
+    sys_role_name varchar(64) NOT NULL
+);
+
+INSERT INTO `system_role`(sys_role_name) VALUES ('User');
