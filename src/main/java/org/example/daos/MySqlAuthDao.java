cffab571df207c5b6aea0dcc77d087a997c85319
thomkainos <thomas.phan@kainos.com>
2024-08-01 11:52:27 -0400

    feat(dao): working on auth dao

--- src/main/java/org/example/daos/MySqlAuthDao.java
+++ src/main/java/org/example/daos/MySqlAuthDao.java
@@ -13,12 +13,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class MySqlAuthDao implements IAuthDao {
     private DatabaseConnector databaseConnector;
 
