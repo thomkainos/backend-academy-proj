79770fb4c76ca4a22d05792534967b4b998e6bf7
thomkainos <thomas.phan@kainos.com>
2024-08-02 09:38:29 -0400

    ci: set up env variable for prod url

--- src/test/java/org/example/integration/AuthIntegrationTest.java
+++ src/test/java/org/example/integration/AuthIntegrationTest.java
@@ -5,6 +5,8 @@ import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import org.example.JobRoleManagerApplication;
 import org.example.JobRoleManagerConfiguration;
 import org.example.models.LoginRequest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,21 +14,32 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
+import javax.ws.rs.core.Response;
+
 
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class AuthIntegrationTest {
+    private String apiUrl;
+
     private static final DropwizardAppExtension<JobRoleManagerConfiguration> APP =
             new DropwizardAppExtension<>(JobRoleManagerApplication.class);
 
-    // FIX ME: Set up env variables for testing with prod vs dev
+    public AuthIntegrationTest() {
+        String apiUrl = System.getenv("API_URL");
+        if (apiUrl == null || apiUrl.isEmpty()) {
+            this.apiUrl = "http://localhost:8080/api/";
+        } else {
+            this.apiUrl = apiUrl;
+        }
+    }
+
     @Test
     void login_shouldReturnJwtTokenWithProperTokenStructure_whenRequestContainsValidCredentials() {
         Client client = APP.client();
         LoginRequest loginRequest = new LoginRequest("user1", "user1");
 
         String response = client
-                .target("http://localhost:8080/api/auth/login")
+                .target(this.apiUrl + "auth/login")
                 .request(MediaType.APPLICATION_JSON)
                 .post(Entity.entity(loginRequest, MediaType.APPLICATION_JSON), String.class);
 
@@ -34,7 +47,15 @@ public class AuthIntegrationTest {
     }
 
     @Test
-    void login_shouldReturnBadRequest_whenRequestContainsInvalidCredentials() {
+    void login_shouldReturnBadRequestCode_whenRequestContainsInvalidCredentials() {
+        Client client = APP.client();
+        LoginRequest invalidLoginRequest = new LoginRequest("invalid", "invalid");
 
+        Response response = client
+                .target("http://localhost:8080/api/auth/login")
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(invalidLoginRequest, MediaType.APPLICATION_JSON), Response.class);
+
+        assertEquals(400, response.getStatus());
     }
 }
