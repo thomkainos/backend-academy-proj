9e7484e30aa7e27f102a4a5f5c8b3eee8c81fd20
thomkainos <thomas.phan@kainos.com>
2024-08-07 11:32:38 -0400

    refactor: updated token expiration time

--- src/main/java/org/example/services/AuthService.java
+++ src/main/java/org/example/services/AuthService.java
@@ -19,8 +19,9 @@ public class AuthService {
     private final IAuthDao authDao;
     private final Key key;
 
-    public String login(final LoginRequest loginRequest) throws InvalidCredentialsException, AuthDaoException {
-        Optional<User> user = authDao.getUser(loginRequest); // change to optional user
+    public String login(final LoginRequest loginRequest) throws
+            InvalidCredentialsException, AuthDaoException {
+        Optional<User> user = authDao.getUser(loginRequest);
 
         if (user.isEmpty()) {
             throw new InvalidCredentialsException();
@@ -30,9 +31,11 @@ public class AuthService {
     }
 
     private String generateJwtToken(final User user) {
+        int tokenExpTimeInMillis = 18000000;
+
         return Jwts.builder()
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + tokenExpTimeInMillis))
                 .claim("Role", user.getSysRoleId())
                 .subject(user.getUsername())
                 .issuer("Kainos Job Role Manager")
