3e91b7fd23053d48299b4c8d11e50acfb75d721c
Sam Walker <samw@kainos.com>
2024-08-09 13:04:10 -0400

    build: trying JWT_KEY env get

--- src/main/java/org/example/JobRoleManagerApplication.java
+++ src/main/java/org/example/JobRoleManagerApplication.java
@@ -44,7 +44,12 @@ public class JobRoleManagerApplication extends Application<JobRoleManagerConfigu
 	}
 
 	private Key getSigningKey() {
-		String signingKeyBase64 = "InNvbWUtcmVhbGx5LWxvbmctdGVzdC1rZXktdGhhdC1pcy1zdXBlci1zZWNyZXQi";
+		String signingKeyBase64;
+		try {
+			signingKeyBase64 = System.getenv().get("JWT_KEY");
+		} catch (NullPointerException e) {
+			throw new RuntimeException("JWT_KEY not set");
+		}
 
 		byte[] keyInBytes = Base64.getDecoder().decode(signingKeyBase64);
 
